8766c29cd517c3508ed0f7a0982a9cbc97fd8146
Razibul Islam <dev.ronib@gmail.com>
2024-05-22 00:31:14 +0600

    UPDATED: Home page updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,17 @@
+import Slider from "./Slider.jsx";
+import Order from "./Order.jsx";
+import Menu from "./Menu.jsx";
+import Featured from "./Featured.jsx";
+
 const Home = () => {
     return (
         <div>
-            Home
+            <Slider/>
+            <div className='w-11/12 lg:w-10/12 max-w-[1275px] mx-auto'>
+                <Order/>
+                <Menu/>
+            </div>
+            <Featured/>
         </div>
     );
 };
