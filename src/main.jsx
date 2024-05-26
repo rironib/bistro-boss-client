e42909c7601c1f3ef91e9770d03c6a93220ddac1
Razibul Islam <dev.ronib@gmail.com>
2024-05-26 23:48:16 +0600

    UPDATED: Fetch data using tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,19 @@ import {HelmetProvider} from "react-helmet-async";
 import router from "./routes/Routes.jsx";
 import './index.css';
 import AuthProvider from "./provider/AuthProvider.jsx";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <AuthProvider>
-            <HelmetProvider>
+            <QueryClientProvider client={queryClient}>
+                <HelmetProvider>
                     <RouterProvider router={router} />
-            </HelmetProvider>
+                </HelmetProvider>
+            </QueryClientProvider>
         </AuthProvider>
     </React.StrictMode>,
 )
