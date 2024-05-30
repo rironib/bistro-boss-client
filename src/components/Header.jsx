c71413ad497e25dd3d37dc83c29ca7c24fd7947f
Razibul Islam <dev.ronib@gmail.com>
2024-05-30 15:24:46 +0600

    Link added to cart icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import {Link, NavLink} from "react-router-dom";
+import {Link, NavLink, useNavigate} from "react-router-dom";
 import {toast} from "react-toastify";
 import useCart from "../hooks/useCart.jsx";
 import useAuth from "../hooks/useAuth.jsx";
@@ -6,10 +6,14 @@ import useAuth from "../hooks/useAuth.jsx";
 const Header = () => {
     const {user, logOut} = useAuth();
     const [cart] = useCart();
+    const navigate = useNavigate();
 
     const handleLogout = () => {
         logOut()
-            .then(() => toast.success("Logout successfully"))
+            .then(() => {
+                toast.success("Logout successfully");
+                navigate('/login');
+            })
             .catch((err) => toast.error(err));
     }
 
@@ -69,7 +73,7 @@ const Header = () => {
                     </div>
                     <div className="flex items-center gap-4">
                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                            <div className="indicator">
+                            <Link to='/dashboard/cart' className="indicator">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none"
                                      viewBox="0 0 24 24"
                                      stroke="currentColor">
@@ -77,7 +81,7 @@ const Header = () => {
                                           d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"/>
                                 </svg>
                                 <span className="badge badge-sm indicator-item">{cart.length}</span>
-                            </div>
+                            </Link>
                         </div>
                         <div className='hidden lg:flex gap-4 font-semibold font-inter uppercase'>
                             {userMenu}
