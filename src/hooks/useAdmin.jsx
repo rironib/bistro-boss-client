84a3ad9ae22609fd64e342a8548b793c9bc864f2
Razibul Islam <dev.ronib@gmail.com>
2024-05-30 18:37:51 +0600

    Create useAdmin.jsx

---
+++ src/hooks/useAdmin.jsx
@@ -0,0 +1,18 @@
+import useAuth from "./useAuth.jsx";
+import useAxiosSecure from "./useAxiosSecure.jsx";
+import {useQuery} from "@tanstack/react-query";
+
+const useAdmin = () => {
+    const {user} = useAuth();
+    const axiosSecure = useAxiosSecure();
+    const {data: isAdmin, isPending: isAdminLoading} = useQuery({
+        queryKey: ['isAdmin', user?.email],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/users/admin/${user.email}`)
+            return res.data?.admin;
+        }
+    })
+    return [isAdmin, isAdminLoading]
+};
+
+export default useAdmin;
